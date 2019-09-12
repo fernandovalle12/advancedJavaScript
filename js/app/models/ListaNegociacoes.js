723af03c566ede4c518d9f8dbe62019ea3450f35
Fernando Valle <fernando.valle@senior.com.br>
2019-09-12 15:19:11 -0300

    usando armadilha para atualizar a view sem precisar dar update

--- js/app/models/ListaNegociacoes.js
+++ js/app/models/ListaNegociacoes.js
@@ -1,17 +1,25 @@
 class ListaNegociacoes {
+
+  constructor(armadilha) {
+
+    this._negociacoes = [];
+    this._armadilha = armadilha;
+  }
+
+  adiciona(negociacao) {
+
+    this._negociacoes.push(negociacao);
+    this._armadilha(this);
+  }
+
+  get negociacoes() {
+
+    return [].concat(this._negociacoes);
+  }
+
+  esvazia() {
     
-    constructor() {
-        
-        this._negociacoes = [];
-    }
-    
-    adiciona(negociacao) {
-        
-        this._negociacoes.push(negociacao);
-    }
-    
-    get negociacoes() {
-        
-        return [].concat(this._negociacoes);
-    }
+    this._negociacoes = [];
+    this._armadilha(this);
+  }
 }
