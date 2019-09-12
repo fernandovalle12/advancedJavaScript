c2f82adbc3c32e57211fab06b9a12f768528afb1
Fernando Valle <fernando.valle@senior.com.br>
2019-09-12 17:03:01 -0300

    adicionando proxy ao projeto

---
+++ js/app/services/ProxyFactory.js
@@ -0,0 +1,18 @@
+class ProxyFactory {
+  
+  static create(objeto, props, acao){
+    
+    return new Proxy(new ListaNegociacoes(), {
+      get(target, prop, receiver) {
+        if(props.includes(prop) && typeof(target[prop]) == typeof(Function)) {
+          return function() {
+
+            Reflect.apply(target[prop], target, arguments);
+            return acao(target);
+          }
+        }
+        return Reflect.get(target, prop, receiver)
+      }
+    });
+  }
+}
